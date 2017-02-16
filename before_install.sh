29347ab42a6b93caf6f7263a12fda6897c527a49
Stefan Heule <stefanheule@gmail.com>
2017-02-16 10:34:47 -0800

    work in progress

--- before_install.sh
+++ before_install.sh
@@ -5,7 +5,7 @@
 npm install npm@3 -g
 
 set -e
-echo 'pBuild 1.4'
+echo 'pBuild 1.5'
 echo 'Installing Pebble SDK and its Dependencies...'
 
 cd ~
@@ -17,12 +17,9 @@ touch ~/.pebble-sdk/ENABLE_ANALYTICS
 # Get the Pebble SDK and toolchain
 if [ ! -d $HOME/pebble-dev/${PEBBLE_SDK} ]; then
   wget https://s3.amazonaws.com/assets.getpebble.com/pebble-tool/${PEBBLE_SDK}.tar.bz2
-  wget http://assets.getpebble.com.s3-website-us-east-1.amazonaws.com/sdk/arm-cs-tools-ubuntu-universal.tar.gz
 
   # Extract the SDK
   tar -jxf ${PEBBLE_SDK}.tar.bz2 -C ~/pebble-dev/
-  # Extract the toolchain
-  tar zxf arm-cs-tools-ubuntu-universal.tar.gz -C ~/pebble-dev/${PEBBLE_SDK}
 
   # Install the Python library dependencies locally
   cd ~/pebble-dev/${PEBBLE_SDK}
