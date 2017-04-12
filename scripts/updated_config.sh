fbb2ec3e9ae806585a59090ea6f859f789027443
Stefan Heule <stefanheule@gmail.com>
2017-04-11 17:11:28 -0700

    fix script

--- scripts/updated_config.sh
+++ scripts/updated_config.sh
@@ -4,8 +4,6 @@ echo "IMPORTANT: Make sure you updated the config branch and pushed to github, o
 
 version=$(cat src/pkjs/index.js |& grep "stefanheule.com/graphite/config/[0-9]" | sed "s/.*config\/\([0-9]*\).*/\1/")
 
-exit 1
-
 echo "Detected config version $version..."
 echo "Resetting config $version on server."
 ssh linode "sudo rm -rf /home/stefan/www/pages/common/data/graphite/config/$version"
